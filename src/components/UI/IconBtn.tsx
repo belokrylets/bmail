e2800c28bf22eedb58e9d59573a24ccffaf5f897
Sergey <belokrylec.s@skdesign.ru>
2023-04-11 16:30:55 +0300

    styles: refactor

--- src/components/UI/IconBtn.tsx
+++ src/components/UI/IconBtn.tsx
@@ -11,6 +11,7 @@ import IconOpen from "assets/icons/IconOpen"
 import IconPen from "assets/icons/IconPen"
 import IconSent from "assets/icons/IconSent"
 import IconSuccess from "assets/icons/IconSuccess"
+import classNames from "classnames"
 import React from "react"
 import { IIconType } from "shared/modals/iconBtn.modal"
 
@@ -38,7 +39,10 @@ const iconMapping = {
 
 const IconBtn: React.FC<IconBtnProps> = ({ onClick, classes, type }) => {
   return (
-    <button className={`icon-btn ${classes}`} onClick={onClick}>
+    <button
+      // className={`icon-btn ${!!classes ? classes : ""}`}
+      className={classNames("icon-btn", { [classes!]: !!classes })}
+      onClick={onClick}>
       {iconMapping[type]}
     </button>
   )
